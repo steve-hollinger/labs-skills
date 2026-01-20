1fb977a85e013a43be53829001c2793a617ced50
Steve Hollinger <s.hollinger@fetchrewards.com>
2026-01-20 16:59:36 -0600

    refactor: Remove remaining examples directories

--- 05-data-databases/neo4j-date-datetime/examples/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Neo4j DATE vs DATETIME examples."""
-
-from .example_1_date import main as example_1_main
-from .example_2_datetime import main as example_2_main
-from .example_3_duration import main as example_3_main
-
-__all__ = ["example_1_main", "example_2_main", "example_3_main"]
